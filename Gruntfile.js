13c1dc0e4da8408f87cf61d505ff91d408600511
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-02-11 22:04:26 +1100

    feat: Responsive images with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+    // Project configuration
+    grunt.initConfig({
+        responsive_images: {
+            dev: {
+                options: {
+                    engine: 'im',
+                    sizes: [{
+                        width: 320,
+                        quality: 60
+                    }, {
+                        width: 640,
+                        quality: 60,
+                    }, {
+                        width: 1024,
+                        quality: 60
+                    }]
+                },
+
+                files: [{
+                    expand: true,
+                    src: ['*.{gif,jpg,png}'],
+                    cwd: 'img_src/',
+                    dest: 'img'
+                }, {
+                    expand: true,
+                    src: ['*.{gif,jpg,png}'],
+                    cwd: 'views/img_src/',
+                    dest: 'views/img'
+                }]
+            }
+        },
+        clean: {
+            dev: {
+                src: ['img', 'views/img']
+            }
+        },
+        mkdir: {
+            dev: {
+                options: {
+                    create: ['img', 'views/img']
+                }
+            }
+        },
+        copy: {
+            dev: {
+                expand: true,
+                cwd: 'views/img_src/fixed/',
+                src: '*.{gif,jpg,png}',
+                dest: 'views/img/'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.registerTask('default', ['clean','mkdir', 'copy', 'responsive_images']);
+};
